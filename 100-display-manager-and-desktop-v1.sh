ca23fa63e48ab0026cce4ae5de81c7b783eca525
khum0r <domagoj.kosmat@gmail.com>
2018-07-21 21:57:43 +0200

    Add files via upload

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -5,7 +5,7 @@ echo "#################################################"
 echo "Installing Window Manager..."
 echo "#################################################"
 
-LIST_OF_APPS="linux-firmware lighdm lightdm-gtk-greeter lightdm-gtk-greeter-settings bspwm sxhkd"
+LIST_OF_APPS="firmware-linux lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings bspwm sxhkd"
 
 sudo apt-get update
 sudo apt-get install -y $LIST_OF_APPS
