68f7fb57cdc77bff010325ec1c93d86565b15b85
khum0r <domagoj.kosmat@gmail.com>
2018-07-22 01:06:37 +0200

    Add files via upload

---
+++ 10-switch-to-testing-v1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+cd ~
+wget https://github.com/khum0r/debian_testing_bspwm/blob/master/sources.list
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bck
+sudo mv sources.list /etc/apt/
+sudo chown root:root /etc/apt/sources.list 
+
+echo "################################################################"
+echo "#########   Switched to Testing                 ################"
+echo "################################################################"
