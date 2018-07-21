424d651a6a41b297a70ee62e218fb09bfac07c52
khum0r <domagoj.kosmat@gmail.com>
2018-07-21 23:38:43 +0200

    Add files via upload

---
+++ 200-software-v1.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+set -e
+
+echo "Installing category Accessories"
+
+#sudo apt-get install -y catfish
+#sudo apt-get install -y cmatrix
+#sudo apt-get install -y galculator
+#sudo apt-get install -y gnome-screenshot
+#sudo apt-get install -y plank
+#sudo apt-get install -y redshift
+#sudo apt-get install -y xfburn
+#sudo apt-get install -y variety
+#sudo apt-get install -y
+
+
+echo "Installing category Development"
+
+#sudo apt-get install -y
+
+echo "Installing category Education"
+
+#sudo apt-get install -y
+
+echo "Installing category Games"
+
+#sudo apt-get install -y
+
+echo "Installing category Graphics"
+
+#sudo apt-get install -y
+
+echo "Installing category Internet"
+
+sudo apt-get install -y firefox-esr
+sudo apt-get install -y weechat weechat-scripts
+#sudo apt-get install -y
+
+echo "Installing category Multimedia"
+
+sudo apt-get install -y mpv
+sudo apt-get install -y vlc
+#sudo apt-get install -y
+
+echo "Installing category Office"
+
+#sudo apt-get install -y libreoffice-fresh
+#sudo apt-get install -y
+
+echo "Installing category Other"
+
+#sudo apt-get install -y
+
+echo "Installing category System"
+
+sudo apt-get install -y curl
+sudo apt-get install -y dmidecode
+sudo apt-get install -y git
+sudo apt-get install -y glances
+sudo apt-get install -y gtk2-engine-murrine
+sudo apt-get install -y gvfs
+sudo apt-get install -y hardinfo
+sudo apt-get install -y hddtemp
+sudo apt-get install -y htop
+sudo apt-get install -y lm-sensors
+sudo apt-get install -y mlocate
+sudo apt-get install -y net-tools
+#sudo apt-get install -y notify-osd
+sudo apt-get install -y numlockx
+sudo apt-get install -y sane
+sudo apt-get install -y screenfetch
+sudo apt-get install -y scrot
+sudo apt-get install -y simple-scan
+sudo apt-get install -y sysstat
+#sudo apt-get install -y terminator
+#sudo apt-get install -y termite
+sudo apt-get install -y thunar
+sudo apt-get install -y thunar-archive-plugin
+sudo apt-get install -y thunar-volman
+sudo apt-get install -y ttf-ubuntu-font-family
+sudo apt-get install -y tumbler
+sudo apt-get install -y vnstat
+sudo apt-get install -y wget
+sudo apt-get install -y unclutter
+sudo apt-get install -y rxvt-unicode
+sudo apt-get install -y urxvt-perls
+#sudo apt-get install -y xdg-user-dirs
+#sudo apt-get install -y
+
+
+###############################################################################################
+
+# installation of zippers and unzippers
+sudo apt-get install -y unace unrar zip unzip sharutils  uudeview  arj cabextract file-roller
+
+###############################################################################################
+
+
+echo "################################################################"
+echo "#### Software installed  #########"
+echo "################################################################"
