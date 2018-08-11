dc3ee4b442f20c380a3d94308aa526b5bbac2d0b
khum0r <domagoj.kosmat@gmail.com>
2018-08-11 03:11:33 +0200

    Update 120-install-polybar-v1.sh

--- 120-install-polybar-v1.sh
+++ 120-install-polybar-v1.sh
@@ -5,7 +5,7 @@ echo "#################################################"
 echo "Installing Polybar..."
 echo "#################################################"
 
-LIST_OF_APPS="build-essentials cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev"
+LIST_OF_APPS="build-essential cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev"
 
 sudo apt-get update
 sudo apt-get install -y $LIST_OF_APPS
